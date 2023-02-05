c1f3ab00c764354d426c7a0aff1b70c90ee6b1d6
Bitjockey <fastjack@jackpoint.network>
2023-02-05 15:12:32 -0500

    Use CaptainDario's fix in dlib

--- lib/src/bindings/dlib.dart
+++ lib/src/bindings/dlib.dart
@@ -1,32 +1,7 @@
 import 'dart:ffi';
 import 'dart:io';
 
-import 'package:path/path.dart' as Path;
-
-const Set<String> _supported = {'linux', 'mac', 'win'};
-
-String get binaryName {
-  String os, ext;
-  if (Platform.isLinux) {
-    os = 'linux';
-    ext = 'so';
-  } else if (Platform.isMacOS) {
-    os = 'mac';
-    ext = 'so';
-  } else if (Platform.isWindows) {
-    os = 'win';
-    ext = 'dll';
-  } else {
-    throw Exception('Unsupported platform!');
-  }
-
-  if (!_supported.contains(os)) {
-    throw UnsupportedError('Unsupported platform: $os!');
-  }
-
-  return 'libtensorflowlite_c-$os.$ext';
-}
-
+/// https://github.com/am15h/tflite_flutter_plugin/issues/199
 /// TensorFlowLite C library.
 // ignore: missing_return
 DynamicLibrary tflitelib = () {
@@ -34,9 +9,19 @@ DynamicLibrary tflitelib = () {
     return DynamicLibrary.open('libtensorflowlite_c.so');
   } else if (Platform.isIOS) {
     return DynamicLibrary.process();
-  } else {    
+  } else if (Platform.isMacOS) {
     return DynamicLibrary.open(
-      Directory(Platform.resolvedExecutable).parent.path + '/blobs/${binaryName}'
-    );
+        Directory(Platform.resolvedExecutable).parent.parent.path +
+            '/resources/libtensorflowlite_c-mac.dylib');
+  } else if (Platform.isLinux) {
+    return DynamicLibrary.open(
+        Directory(Platform.resolvedExecutable).parent.path +
+            '/blobs/libtensorflowlite_c-linux.so');
+  } else if (Platform.isWindows) {
+    return DynamicLibrary.open(
+        Directory(Platform.resolvedExecutable).parent.path +
+            '/blobs/libtensorflowlite_c-win.dll');
+  } else {
+    throw UnsupportedError('Unsupported platform!');
   }
 }();
